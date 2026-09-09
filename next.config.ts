f7a874494954b9f84ef9dec37f1181619f226e9a
Sean Kirk <seankirk757@gmail.com>
2026-09-10 00:50:02 +0100

    Whitelisted website in order to pass URL into the Next.js <Image> component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.139", "localhost"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'resources.premierleague.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
