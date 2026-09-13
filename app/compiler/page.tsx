779d821fdba3f49eaf8dbdd1626a31e550543e91
Sean Kirk <seankirk757@gmail.com>
2026-09-13 22:19:20 +0100

    Updated header of compiler page

--- app/compiler/page.tsx
+++ app/compiler/page.tsx
@@ -22,24 +22,9 @@ export default async function CompilerPage() {
 
   return (
     <div className="h-dvh overflow-hidden bg-[#00e5ff] text-black py-4 px-2 w-screen max-w-379 min-w-[320px] flex flex-col gap-3">
-      
-      {/* Top Header with Back Button */}
-      <div className="shrink-0 flex items-center justify-between">
-        <Link 
-          href="/" 
-          className="bg-white/50 hover:bg-white/80 transition text-black font-extrabold text-xs px-3 py-2 rounded-md"
-        >
-          &larr; Back
-        </Link>
-        <h1 className="text-3xl font-medium tracking-tight pr-4">
-          Compiler
-        </h1>
-        <div className="w-10" /> {/* Empty div to balance flex spacing */}
-      </div>
-
       {/* Main Content Area */}
       {players.length > 0 ? (
-        <div className="flex-1 min-h-0 flex flex-col">
+        <div className="flex-1 min-h-0 flex flex-col gap-2">
           <CompilerTable players={players} teams={teams} positions={positions} />
         </div>
       ) : (
